ee71352906d583a311c438a5e0881dbeabd45575
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2021-03-27 21:17:14 +0000

    Fix first content paint

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
   const rec = document.querySelector('#rectangle');
   let rotation = 0;
 
+  rec.style.setProperty('--rot', rotation + 'deg');
+
   document.addEventListener('pointermove', event => {
     rotation += Math.sqrt(event.movementX ** 2 + event.movementY ** 2) / 10;
 
